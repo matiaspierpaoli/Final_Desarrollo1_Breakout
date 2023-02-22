f72d468c758126b9fe6c68d70a248989f2de77db
matiaspierpaoli <90465662+matiaspierpaoli@users.noreply.github.com>
2023-02-22 13:34:29 -0300

    Creado el cpp de Menu y sus métodos vacíos

---
+++ Final_Desarrollo1_Pierpaoli/src/Menu.cpp
@@ -0,0 +1,26 @@
+#include "Menu.h"
+
+Menu::Menu(SceneManager* sceneManager)
+{
+
+}
+
+Menu::~Menu()
+{
+
+}
+
+void Menu::Update()
+{
+
+}
+
+void Menu::Draw()
+{
+
+}
+
+void Menu::Update()
+{
+
+}
