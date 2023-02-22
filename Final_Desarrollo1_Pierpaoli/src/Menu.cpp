fbcfde046354c8088e9a22eed4146dc5a94813f7
matiaspierpaoli <90465662+matiaspierpaoli@users.noreply.github.com>
2023-02-22 15:27:48 -0300

    Correcto dibujado de titulo, play y exit en menú con detección de input

--- Final_Desarrollo1_Pierpaoli/src/Menu.cpp
+++ Final_Desarrollo1_Pierpaoli/src/Menu.cpp
@@ -4,15 +4,20 @@ Menu::Menu(SceneManager* sceneManager)
 {
 	this->sceneManager = sceneManager;
 
+	titleBox.width = 400;
+	titleBox.height = 75;
+	titleBox.x = GetScreenWidth() / 2 - titleBox.width / 2;
+	titleBox.y = 50;
+
 	playBox.width = 150;
 	playBox.height = 75;
-	playBox.x = GetScreenWidth() / 2 - 100;
+	playBox.x = GetScreenWidth() / 2 - playBox.width / 2;
 	playBox.y = GetScreenHeight() / 2 - 100;
 
 	exitBox.width = 150;
 	exitBox.height = 75;
-	exitBox.x = GetScreenWidth() / 2 - 100;
-	exitBox.y = GetScreenHeight() - 200;
+	exitBox.x = GetScreenWidth() / 2 - exitBox.width / 2;
+	exitBox.y = GetScreenHeight() - 180;
 }
 
 Menu::~Menu()
@@ -20,12 +25,22 @@ Menu::~Menu()
 
 }
 
-void Menu::Update()
+void Menu::UpdateAndDraw()
 {
+	BeginDrawing();
+	ClearBackground(BLACK);
 
-}
+	DrawText("Breakout ", titleBox.x, titleBox.y, 100, RED);
 
-void Menu::Draw()
-{
+	if (CheckCollisionPointRec(GetMousePosition(), playBox)) DrawText("Play", playBox.x, playBox.y, 80, RED);
+	else  DrawText(FormatText("Play"), playBox.x, playBox.y, 80, WHITE);
 
+	if (CheckCollisionPointRec(GetMousePosition(), exitBox)) DrawText("Exit", exitBox.x, exitBox.y, 80, RED);
+	else  DrawText(FormatText("Exit"), exitBox.x, exitBox.y, 80, WHITE);
+
+
+	if (CheckCollisionPointRec(GetMousePosition(), playBox) && IsMouseButtonReleased(MOUSE_LEFT_BUTTON)) sceneManager->setScene(Scene::GAMEPLAY);
+	if (CheckCollisionPointRec(GetMousePosition(), exitBox) && IsMouseButtonReleased(MOUSE_LEFT_BUTTON)) sceneManager->setScene(Scene::EXIT);
+
+	EndDrawing();
 }
--- Final_Desarrollo1_Pierpaoli/src/Menu.h
+++ Final_Desarrollo1_Pierpaoli/src/Menu.h
@@ -7,6 +7,7 @@ class Menu
 private:
 	SceneManager* sceneManager;
 
+	Rectangle titleBox;
 	Rectangle playBox;
 	Rectangle exitBox;
 	
@@ -14,7 +15,6 @@ private:
 public:
 	Menu(SceneManager* sceneManager);
 	~Menu();
-	void Update();
-	void Draw();
+	void UpdateAndDraw();
 	
 };
