510d5bb6a45e0c59b9bf7ae2d593d8818df781aa
matiaspierpaoli <90465662+matiaspierpaoli@users.noreply.github.com>
2023-02-22 15:44:45 -0300

    Dibujado y update de exit en rules

--- Final_Desarrollo1_Pierpaoli/src/Rules.cpp
+++ Final_Desarrollo1_Pierpaoli/src/Rules.cpp
@@ -3,6 +3,11 @@
 Rules::Rules(SceneManager* sceneManager)
 {
 	this->sceneManager = sceneManager;
+
+	exitBox.width = 150;
+	exitBox.height = 75;
+	exitBox.x = GetScreenWidth() / 2 - exitBox.width / 2;
+	exitBox.y = GetScreenHeight() - 180;
 }
 
 Rules::~Rules()
@@ -15,6 +20,11 @@ void Rules::DrawAndUpdate()
 	BeginDrawing();
 
 	ClearBackground(BLACK);
+
+	if (CheckCollisionPointRec(GetMousePosition(), exitBox)) DrawText("Exit", exitBox.x, exitBox.y, 80, RED);
+	else  DrawText(FormatText("Exit"), exitBox.x, exitBox.y, 80, WHITE);
+
+	if (CheckCollisionPointRec(GetMousePosition(), exitBox) && IsMouseButtonReleased(MOUSE_LEFT_BUTTON)) sceneManager->setScene(Scene::MENU);
 
 	EndDrawing();
 }
