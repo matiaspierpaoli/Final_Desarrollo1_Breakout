630e580bd0db7a1f691020fac52958b11b70170b
matiaspierpaoli <90465662+matiaspierpaoli@users.noreply.github.com>
2023-02-22 16:03:32 -0300

    Modificado el tamaño de exit en todas las escenas (excepto GAMEPLAY)

--- Final_Desarrollo1_Pierpaoli/src/Credits.cpp
+++ Final_Desarrollo1_Pierpaoli/src/Credits.cpp
@@ -4,10 +4,10 @@ Credits::Credits(SceneManager* sceneManager)
 {
 	this->sceneManager = sceneManager;
 
-	exitBox.width = 150;
-	exitBox.height = 75;
+	exitBox.width = 120;
+	exitBox.height = 50;
 	exitBox.x = GetScreenWidth() / 2 - exitBox.width / 2;
-	exitBox.y = GetScreenHeight() - 180;
+	exitBox.y = GetScreenHeight() - 100;
 }
 
 Credits::~Credits()
@@ -21,8 +21,8 @@ void Credits::UpdateAndDraw()
 
 	ClearBackground(BLACK);
 
-	if (CheckCollisionPointRec(GetMousePosition(), exitBox)) DrawText("Exit", exitBox.x, exitBox.y, 80, RED);
-	else  DrawText(FormatText("Exit"), exitBox.x, exitBox.y, 80, WHITE);
+	if (CheckCollisionPointRec(GetMousePosition(), exitBox)) DrawText("Exit", exitBox.x, exitBox.y, 50, RED);
+	else  DrawText(FormatText("Exit"), exitBox.x, exitBox.y, 50, WHITE);
 
 	if (CheckCollisionPointRec(GetMousePosition(), exitBox) && IsMouseButtonReleased(MOUSE_LEFT_BUTTON)) sceneManager->setScene(Scene::MENU);
 
--- Final_Desarrollo1_Pierpaoli/src/Menu.cpp
+++ Final_Desarrollo1_Pierpaoli/src/Menu.cpp
@@ -4,17 +4,17 @@ Menu::Menu(SceneManager* sceneManager)
 {
 	this->sceneManager = sceneManager;
 
-	titleBox.width = 400;
+	titleBox.width = 450;
 	titleBox.height = 75;
 	titleBox.x = GetScreenWidth() / 2 - titleBox.width / 2;
 	titleBox.y = 50;
 
-	playBox.width = 150;
+	playBox.width = 180;
 	playBox.height = 75;
 	playBox.x = GetScreenWidth() / 2 - playBox.width / 2;
 	playBox.y = GetScreenHeight() / 2 - 100;
 
-	rulesBox.width = 200;
+	rulesBox.width = 230;
 	rulesBox.height = 75;
 	rulesBox.x = GetScreenWidth() / 2 - rulesBox.width / 2;
 	rulesBox.y = GetScreenHeight() / 2;
@@ -24,10 +24,10 @@ Menu::Menu(SceneManager* sceneManager)
 	creditsBox.x = GetScreenWidth() / 2 - creditsBox.width / 2;
 	creditsBox.y = GetScreenHeight() / 2 + 100;
 
-	exitBox.width = 150;
-	exitBox.height = 75;
+	exitBox.width = 120;
+	exitBox.height = 50;
 	exitBox.x = GetScreenWidth() / 2 - exitBox.width / 2;
-	exitBox.y = GetScreenHeight() - 180;
+	exitBox.y = GetScreenHeight() - 100;
 }
 
 Menu::~Menu()
@@ -51,8 +51,8 @@ void Menu::UpdateAndDraw()
 	if (CheckCollisionPointRec(GetMousePosition(), creditsBox)) DrawText("Credits", creditsBox.x, creditsBox.y, 80, RED);
 	else  DrawText(FormatText("Credits"), creditsBox.x, creditsBox.y, 80, WHITE);
 
-	if (CheckCollisionPointRec(GetMousePosition(), exitBox)) DrawText("Exit", exitBox.x, exitBox.y, 80, RED);
-	else  DrawText(FormatText("Exit"), exitBox.x, exitBox.y, 80, WHITE);
+	if (CheckCollisionPointRec(GetMousePosition(), exitBox)) DrawText("Exit", exitBox.x, exitBox.y, 50, RED);
+	else  DrawText(FormatText("Exit"), exitBox.x, exitBox.y, 50, WHITE);
 
 
 	if (CheckCollisionPointRec(GetMousePosition(), playBox) && IsMouseButtonReleased(MOUSE_LEFT_BUTTON)) sceneManager->setScene(Scene::GAMEPLAY);
